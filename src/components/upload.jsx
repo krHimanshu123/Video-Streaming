dec9ad8c89eb3d17be16a2db0ff750e4267f3dac
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-05-11 07:30:20 +0530

    Update upload.jsx

--- src/components/upload.jsx
+++ src/components/upload.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Upload.css';
+import './upload.css';
 
 function Upload() {
   const navigate = useNavigate();
