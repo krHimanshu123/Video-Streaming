fab127d3e2df7009d1917f4b8bffc672e9e6c5c5
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-31 23:24:44 +0530

    Update JwtUtils.java

--- src/main/java/jar/authentication/JwtUtils.java
+++ src/main/java/jar/authentication/JwtUtils.java
@@ -15,6 +15,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
+
 @Component
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
