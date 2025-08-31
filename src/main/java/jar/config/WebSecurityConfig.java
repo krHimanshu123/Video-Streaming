9082d6fd7ea6d09a2f2b9ddbaa4a0104bd90185d
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-31 23:27:53 +0530

    Update WebSecurityConfig.java

--- src/main/java/jar/config/WebSecurityConfig.java
+++ src/main/java/jar/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 import jakarta.servlet.Filter;
 
 import java.util.Arrays;
