1ce17969bfeb7511807cca3bae2ed5c2a1545ff1
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-09-13 00:16:26 +0530

    Update Signup.jsx

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Auth.css';
 
-
 function Signup() {
   const [formData, setFormData] = useState({
     username: '',
