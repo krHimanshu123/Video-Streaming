5b80f2b266a1c78323f98b727a667e747cec2b81
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-09-13 00:17:57 +0530

    Update upload.jsx

--- src/components/upload.jsx
+++ src/components/upload.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './upload.css';
 
+
 function Upload() {
   const navigate = useNavigate();
   const [videoDetails, setVideoDetails] = useState({
